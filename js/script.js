cc754022e69c317c06feea453317e6e8158c0740
Artem <skrartrom@icloud.com>
2020-09-06 22:32:59 +0300

    structure was changed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-console.log('Everything work correct!')
+let hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
+let nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
 
-// * important
-// ! error or do not use
-// ? question
-// TODO task
+hamburger_icon.addEventListener('click', () => {
+	nav_list.classList.toggle('open');
+	hamburger_icon.classList.toggle('active');
+});
